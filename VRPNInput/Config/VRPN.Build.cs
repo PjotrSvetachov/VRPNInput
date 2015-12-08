f4280b7d49f34a933c1ee0c23ef2df92078f9d18
Pjotr Svetachov <pjotrsvetachov@gmail.com>
2015-12-08 15:15:00 +0100

    Added VRPN.Build.cs, this file is used to let UE4 build tools know about the VRPN library.

---
+++ VRPNInput/Config/VRPN.Build.cs
@@ -0,0 +1,28 @@
+﻿
+using UnrealBuildTool;
+using System;
+using System.IO;
+
+public class VRPN : ModuleRules
+{
+    public VRPN(TargetInfo Target)
+	{
+		Type = ModuleType.External;
+
+        string basePath = Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name));
+        
+        PublicSystemIncludePaths.Add(Path.Combine(basePath, "include"));
+
+        if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+            PublicLibraryPaths.Add(Path.Combine(basePath, "lib", "win64"));
+            PublicAdditionalLibraries.Add("vrpn.lib");
+		}
+        else if (Target.Platform == UnrealTargetPlatform.Linux)
+        {
+            PublicLibraryPaths.Add(Path.Combine(basePath, "lib", "linux"));
+            PublicAdditionalLibraries.Add(Path.Combine(basePath, "lib", "linux", "vrpn.a"));
+            PublicAdditionalLibraries.Add(Path.Combine(basePath, "lib", "linux", "quat.a"));
+        }
+	}
+}
